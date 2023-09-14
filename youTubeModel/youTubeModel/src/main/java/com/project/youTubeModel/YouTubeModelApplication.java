a9498d4196054a96b7b8eab44964036496f25bac
Balamurali03 <balamurali03101997@gmail.com>
2023-09-14 11:15:46 +0530

    intialcommit Code Push

---
+++ youTubeModel/youTubeModel/src/main/java/com/project/youTubeModel/YouTubeModelApplication.java
@@ -0,0 +1,13 @@
+package com.project.youTubeModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeModelApplication.class, args);
+	}
+
+}
---
+++ youTubeModel/youTubeModel/src/test/java/com/project/youTubeModel/YouTubeModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.youTubeModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
